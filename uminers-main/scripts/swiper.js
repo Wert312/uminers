f08e03527f889e35bdaac13e5b264fc27038d67b
Wert312 <verhushaand456@mail.ru>
2023-10-02 15:12:30 +0700

    fixed sliders index.html

--- uminers-main/scripts/swiper.js
+++ uminers-main/scripts/swiper.js
@@ -11,8 +11,8 @@ const swiper = new Swiper('.swiper1', {
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.experts__navigation--button-next',
+        prevEl: '.experts__navigation--button-prev',
     },
     breakpoints: {
         320: {
@@ -31,7 +31,7 @@ const swiper = new Swiper('.swiper1', {
     }
 });
 
-const swiperTeam = new Swiper('.swiper2', {
+const swiperAchievements = new Swiper('.swiper2', {
     // Optional parameters
     direction: 'horizontal',
 
@@ -44,8 +44,41 @@ const swiperTeam = new Swiper('.swiper2', {
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.achievements__navigation--button-next',
+        prevEl: '.achievements__navigation--button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 14
+        },
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 14
+        }
+    }
+});
+
+const swiperTeam = new Swiper('.swiper3', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination3',
+        type: `bullets`,
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.team__navigation--button-next',
+        prevEl: '.team__navigation--button-prev',
     },
     breakpoints: {
         320: {
